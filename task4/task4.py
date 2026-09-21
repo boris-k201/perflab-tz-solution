ed0b668dc45c4931259842f754f65a47733641b0
Boris Kolesnikov <barbariskolesnikov@gmail.com>
2026-09-21 17:16:17 +0300

    solve task 4

---
+++ task4/task4.py
@@ -0,0 +1,27 @@
+from sys import argv
+
+def readNumbers(path):
+    numbers = []
+    with open(path, 'rt') as file:
+        numbers = [int(line.strip()) for line in file]
+    return numbers
+
+def main():
+    if len(argv) < 2:
+        print(f"{argv[0]}: <numbers.txt>")
+    pathNumbers = argv[1]
+
+    numbers = readNumbers(pathNumbers)
+    numbers.sort()
+
+    median = numbers[len(numbers)//2]
+    numbers = [abs(number - median) for number in numbers]
+    sumNumbers = sum(numbers)
+
+    if sumNumbers > 20:
+        print("20 ходов недостаточно для приведения всех элементов массива к одному числу")
+    else:
+        print(sumNumbers)
+
+if __name__ == '__main__':
+    main()
